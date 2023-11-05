e3ada12b6db2443cf2e78be9b69666e5dcfbf9bc
Ya-Jhao Chen <guirilchen@gmail.com>
2023-11-05 17:56:11 +0800

    Add mouse drag for horizontal scrolling in hourly weather section

--- src/components/weather/HourlyWeather.vue
+++ src/components/weather/HourlyWeather.vue
@@ -10,6 +10,7 @@ const props = defineProps<{
 }>();
 
 const scrollY = ref<number>(0);
+const dailyWeatherList = ref<HTMLUListElement | null>(null);
 
 const formatHours = (time: string) => {
   const weatherDate = getDateFromFullTime(time);
@@ -55,6 +56,33 @@ const handleScroll = () => {
   scrollY.value = window.scrollY;
 };
 
+// horizontal scrolling is inspired by https://codepen.io/toddwebdev/pen/yExKoj
+let isMousedown = false;
+let startX: number;
+let scrollLeft: number;
+
+const handleMousedown = (evt: MouseEvent) => {
+  if (!dailyWeatherList.value) return;
+
+  isMousedown = true;
+  startX = evt.pageX - dailyWeatherList.value.offsetLeft;
+  scrollLeft = dailyWeatherList.value.scrollLeft;
+};
+
+const handleMousemove = (evt: MouseEvent) => {
+  if (!isMousedown || !dailyWeatherList.value) return;
+
+  evt.preventDefault();
+
+  const x = evt.pageX - dailyWeatherList.value.offsetLeft;
+  const distance = (x - startX) * 1.5;
+
+  dailyWeatherList.value.scrollLeft = scrollLeft - distance;
+};
+
+const handleMouseup = () => (isMousedown = false);
+const handleMouseleave = () => (isMousedown = false);
+
 onMounted(() => {
   window.addEventListener('scroll', handleScroll);
 });
@@ -68,7 +96,7 @@ onUnmounted(() => {
 .relative.pt-14.mb-8.bg-primary.rounded-md
   .w-full.rounded-t-md(class="max-w-[calc(100vw-64px)] sm:max-w-[640px] bg-primary" :style="titleStyle")
     span.block.pt-2.pb-5.pl-2.text-lg.font-bold.text-white.rounded-t-md(class="bg-white/60") HOURLY FORECAST
-  ul.flex.pb-2.px-2.overflow-x-auto.scrollbar-hide(class="bg-white/60")
+  ul.flex.pb-2.px-2.cursor-pointer.overflow-x-auto.scrollbar-hide(class="bg-white/60" ref="dailyWeatherList" @mousedown="handleMousedown" @mousemove="handleMousemove" @mouseup="handleMouseup" @mouseleave="handleMouseleave")
     li.flex.flex-col.items-center.mr-4.text-zinc-800(class="last:mr-0" v-for="hour in hourlyWeather" :key="hour.time_epoch")
       template(v-if="hour.condition")
         span.text-lg.font-medium {{ formatHours(hour.time) }}
