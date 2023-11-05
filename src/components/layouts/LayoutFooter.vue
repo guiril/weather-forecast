4d5b3b525a07d6cbfab28c3f8bac10b26cfacc7b
Ya-Jhao Chen <guirilchen@gmail.com>
2023-11-05 16:27:16 +0800

    Add sunset icons attribution

--- src/components/layouts/LayoutFooter.vue
+++ src/components/layouts/LayoutFooter.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-footer.fixed.bottom-0.z-20.w-full.py-6.bg-white
-  a(href="https://www.weatherapi.com/" title="Free Weather API")
-    img.block.mx-auto(src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0")
+footer.fixed.bottom-0.z-20.w-full.py-2.bg-white
+  .flex.flex-col
+    a.mb-2(href="https://www.weatherapi.com/" title="Free Weather API")
+      img.block.mx-auto(src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0")
+    a.text-center.underline(href="https://www.flaticon.com/free-icons/sunset" title="sunset icons") Sunset icons created by Icon Hubs - Flaticon
 </template>
